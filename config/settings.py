b8afe1c9d1f0754cc6855fb5e708a136359f9542
OlegNikulinSAPP <oleg309@yandex.ru>
2026-04-27 23:06:10 +0300

    Fix static files collection

--- config/settings.py
+++ config/settings.py
@@ -119,8 +119,10 @@ USE_I18N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+# Static files
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # куда собирается статика
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # откуда брать
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
