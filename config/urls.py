613c4ffae5c9bdfb8c54ea81ae1afe6474d0fc79
OlegNikulinSAPP <oleg309@yandex.ru>
2026-04-28 09:35:39 +0300

    Add static URL patterns for DEBUG mode

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include, re_path  # вместо url используем re_path
 # from .swagger import schema_view
 # from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -35,3 +37,6 @@ urlpatterns = [
     # path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     # path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
