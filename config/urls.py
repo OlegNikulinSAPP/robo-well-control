e805e072c7b7ebfc70a922b155acf52d8ac28769
OlegNikulinSAPP <oleg309@yandex.ru>
2026-07-07 15:32:00 +0300

    Serve static files via Django urls

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
+from django.conf.urls.static import static
 
 # Правильный способ обернуть админку в csrf_exempt
 urlpatterns = [
-    path('admin/', csrf_exempt(admin.site.urls)),  # ← ТАК ПРАВИЛЬНО!
+    path('admin/', csrf_exempt(admin.site.urls)),
     path('api/', include('core.urls')),
     path('', include('core.urls_web')),
 ]
+
+# Добавляем обслуживание статики для всех случаев
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
