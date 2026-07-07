c1fc865ba2af323c87acf19fda7bde794be82f7f
OlegNikulinSAPP <oleg309@yandex.ru>
2026-07-07 08:02:57 +0300

    Disable CSRF for admin via urls.py

--- config/urls.py
+++ config/urls.py
@@ -22,7 +22,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    path('admin/', include('config.admin_urls')),
     path('api/', include('core.urls')),
     path('', include('core.urls_web')),
 
