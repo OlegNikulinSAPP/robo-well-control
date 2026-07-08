017adc31bcb08ca2eb0b8c607b8ff47d92551542
OlegNikulinSAPP <oleg309@yandex.ru>
2026-07-08 15:05:23 +0300

    Fix admin csrf_exempt

--- config/urls.py
+++ config/urls.py
@@ -1,14 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.decorators.csrf import csrf_exempt
-from core.admin_views import backup_list, backup_download, backup_create, backup_restore
-
-# Правильный способ обернуть админку
-admin_site = admin.site
-admin_site.login = csrf_exempt(admin_site.login)
 
 urlpatterns = [
-    path('admin/', admin_site.urls),
+    path('admin/', csrf_exempt(admin.site.urls)),  # ← ТАК ПРАВИЛЬНО!
     path('api/', include('core.urls')),
     path('', include('core.urls_web')),
 ]
